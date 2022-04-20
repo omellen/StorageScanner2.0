dca00ab6cdcc8815c1e14a98191f8b8c174e48df
omellen <olivia.mellen@live.com>
2022-04-20 12:52:27 -0500

    Initial Commit

---
+++ StorageScanner2.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StorageScanner2.0
+//
+//  Created by Olivia Mellen on 4/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
